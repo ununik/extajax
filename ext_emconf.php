4bb5a57d6c2202a1e9f8fe0a355aac0dbf39af39
Martin <martin@mail.com>
2015-08-28 09:44:20 +0200

    initial files (ext_emconf and ext_icon)

---
+++ ext_emconf.php
@@ -0,0 +1,29 @@
+<?php
+$EM_CONF[$_EXTKEY] = array(
+    'title' => 'Somethingwhatever',
+    'description' => 'An extension for getting parts from form via ajax',
+    'category' => 'plugin',
+    'author' => 'Martin Pribyl',
+    'shy' => '',
+    'priority' => '',
+    'module' => '',
+    'state' => 'stable',
+    'internal' => '',
+    'uploadfolder' => '0',
+    'createDirs' => '',
+    'modify_tables' => '',
+    'clearCacheOnLoad' => 0,
+    'lockType' => '',
+    'version' => '1.0.2',
+    'constraints' => array(
+        'depends' => array(
+            'php' => '5.3.7-5.6.999',
+            'typo3' => '6.0.0-7.99.99',
+        ),
+        'conflicts' => array(
+        ),
+        'suggests' => array(
+        ),
+    ),
+);
+?>
